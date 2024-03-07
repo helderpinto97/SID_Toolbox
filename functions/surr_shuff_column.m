f95646f8d7164db34d4d68f2bd456486b7a6c376
Hélder Pinto <124047727+helderpinto97@users.noreply.github.com>
2024-03-07 17:13:46 +0100

    Add files via upload

---
+++ functions/surr_shuff_column.m
@@ -0,0 +1,10 @@
+function [B] = surr_shuff_column(B,ii)
+%Performs joint random permutation of [ii] columns of 2D matrix
+
+if ~exist('ii','var'), ii=1; end
+
+p=randperm(size(B,1));
+B(p,ii) = B(:,ii);
+
+end
+
